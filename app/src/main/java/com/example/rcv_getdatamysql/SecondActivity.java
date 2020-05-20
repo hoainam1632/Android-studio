9202651149eeaebdb66795613d842686fdf1f642
Hoai Nam <namnguyenhoai350@gmail.com>
2020-05-20 09:39:41 +0700

    get data from Json  second Activity

--- app/src/main/java/com/example/rcv_getdatamysql/SecondActivity.java
+++ app/src/main/java/com/example/rcv_getdatamysql/SecondActivity.java
@@ -1,13 +1,39 @@
 package com.example.rcv_getdatamysql;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private TextView txtHeader;
+    private ImageView img2;
+    String getName, getImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        txtHeader = (TextView) findViewById(R.id.txtheader);
+        img2 = (ImageView) findViewById(R.id.imgView);
+        getdata();
+        setData();
+    }
+    private void getdata() {
+        Intent intent = getIntent();
+        if (intent.hasExtra("name") && intent.hasExtra("img")){
+            getName = intent.getStringExtra("name");
+            getImg = intent.getStringExtra("img");
+        }else Toast.makeText(this, "No has data", Toast.LENGTH_SHORT).show();
+    }
+    private void setData() {
+        txtHeader.setText(getName);
+        Picasso.get().load(getImg)
+                .resize(1100,600)
+                .into(img2);
     }
 }
